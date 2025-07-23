56abb5b4945e769395db48eb070a92e70668419e
VincebusRiveruptum <vincebus@Vincebus-PC.>
2025-07-22 21:27:38 -0400

    ADD: Navbar scroll animation

--- src/components/layouts/PublicLayout.vue
+++ src/components/layouts/PublicLayout.vue
@@ -1,12 +1,47 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { Observer } from "tailwindcss-intersect";
+
 import AppNavbar from "@/components/shared/AppNavbar.vue";
 import AppFooter from "@/components/shared/AppFooter.vue";
+import { onMounted, useTemplateRef, watch } from "vue";
+
+import { useScroll } from "@vueuse/core";
+
+const mainWindow = useTemplateRef<HTMLElement>("main");
+const { isScrolling } = useScroll(mainWindow);
+const hideNavbar = ref<boolean>(false);
+
+let timeout: ReturnType<typeof setTimeout> | null = null;
+watch(
+  () => isScrolling.value,
+  (newValue) => {
+    if (newValue === true) {
+      hideNavbar.value = true;
+    } else {
+      if (timeout) clearTimeout(timeout);
+
+      timeout = setTimeout(() => {
+        hideNavbar.value = false;
+      }, 1000);
+    }
+  }
+);
+
+onMounted(() => {
+  Observer.start();
+});
 </script>
 
 <template>
-  <div class="flex flex-col h-screen overflow-clip">
-    <AppNavbar />
-    <div class="flex flex-col h-full overflow-auto">
+  <div class="relative flex flex-col h-screen overflow-clip">
+    <AppNavbar
+      :class="[
+        hideNavbar ? '-translate-y-2' : '',
+        'transition-all duration-500 delay-100 absolute z-25 top-0',
+      ]"
+    />
+    <div ref="main" class="flex flex-col h-full overflow-auto -mt-2">
       <div class="grow">
         <router-view />
       </div>
--- src/modules/landing/components/sections/SkillsSection.vue
+++ src/modules/landing/components/sections/SkillsSection.vue
@@ -36,17 +36,61 @@ import AppTechButton from "@/components/shared/AppTechButton.vue";
         </div>
 
         <div class="flex flex-row flex-wrap justify-center gap-4">
-          <AppTechButton size="xl" :tech="techs[8]" />
-          <AppTechButton size="xl" :tech="techs[9]" />
-          <AppTechButton size="xl" :tech="techs[14]" />
-          <AppTechButton size="xl" :tech="techs[10]" />
-          <AppTechButton size="xl" :tech="techs[3]" />
-          <AppTechButton size="xl" :tech="techs[25]" />
-          <AppTechButton size="xl" :tech="techs[2]" />
-          <AppTechButton size="xl" :tech="techs[26]" />
-          <AppTechButton size="xl" :tech="techs[0]" />
-          <AppTechButton size="xl" :tech="techs[24]" />
-          <AppTechButton size="xl" :tech="techs[6]" />
+          <AppTechButton
+            size="xl"
+            :tech="techs[8]"
+            class="intersect:motion-preset-slide-up motion-delay-50"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[9]"
+            class="intersect:motion-preset-slide-up motion-delay-150"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[14]"
+            class="intersect:motion-preset-slide-up motion-delay-250"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[10]"
+            class="intersect:motion-preset-slide-up motion-delay-300"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[3]"
+            class="intersect:motion-preset-slide-up motion-delay-450"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[25]"
+            class="intersect:motion-preset-slide-up motion-delay-500"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[2]"
+            class="intersect:motion-preset-slide-up motion-delay-700"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[26]"
+            class="intersect:motion-preset-slide-up motion-delay-780"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[0]"
+            class="intersect:motion-preset-slide-up motion-delay-890"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[24]"
+            class="intersect:motion-preset-slide-up motion-delay-1000"
+          />
+          <AppTechButton
+            size="xl"
+            :tech="techs[6]"
+            class="intersect:motion-preset-slide-up motion-delay-1100"
+          />
         </div>
       </div>
     </div>
